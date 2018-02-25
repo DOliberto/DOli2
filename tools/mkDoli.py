528ac48ade8681620aacfbe9b6844ba8dc32dc2e
odanoburu <bcclaro@gmail.com>
2018-02-25 15:31:30 -0300

    - update test.html with new syntax
    - mkDoli producing 'AST'

--- tools/mkDoli.py
+++ tools/mkDoli.py
@@ -1,5 +1,5 @@
 from lxml import etree
-from functools import filter, reduce
+from functools import reduce
 
 """
 - [] add logging http://docs.python-guide.org/en/latest/writing/logging/
@@ -9,19 +9,49 @@ from functools import filter, reduce
 
 # XPATH to elements marked up with DOli form information
 DOLI_XPATH = "//span[@class='preprocdoli']"
+VALIDATOR_LIST_SEP = "|"
+VALIDATOR_MAP = {"not-empty": "DataRequired()", "is-number": "Number()"}
 
-def finfo_is_valid(finfo):
-    return True
+def elem_valid(elem):
+    try:
+        assert elem.get("class") == "preprocdoli"
+        assert elem.get("data-label") is not None
+        assert elem.get("data-validator") is not None
+        assert elem.get("data-long") in [None, "true", "false"]
+        return True
+    except AssertionError:
+        return False
+
+def is_long(val):
+    if val is None:
+        return False
+    val = val.lower()
+    if val == "true":
+        return True
+    elif val == "false":
+        return False
+
+def elem2finfo(elem):
+    if elem_valid(elem):
+        return (is_long(elem.get("data-long")), elem.get("data-label"), elem_validators(elem.get("data-validator")))
+    else:
+        return None
+
+def to_val(validator_str):
+    return VALIDATOR_MAP[validator_str]
+
+def elem_validators(val_field):
+    val_elems = val_field.split(VALIDATOR_LIST_SEP)
+    return list(map(to_val, val_elems))
 
 def compile_forms_in_file(fp, p):
     html = etree.parse(fp, parser=p)
     fs_infos = html.findall(DOLI_XPATH)
-    return filter(finfo_is_valid, fs_infos)
+    return list(map(elem2finfo, fs_infos))
 
 def compile_files(fps, p):
     return reduce(lambda finfos, fp: compile_forms_in_file(fp, p) + finfos, fps, [])
 
 def compile_forms(files):
-    html_parser = etree.HTMLParser()
-    return compile_files(fps, html_parser)
-        
+    html_parser = etree.HTMLParser(encoding="utf8", recover=False)
+    return compile_files(files, html_parser)
