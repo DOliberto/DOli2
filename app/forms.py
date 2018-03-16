189ee112d41a4a95ad02d47008766a7150eb9b7b
odanoburu <bcclaro@gmail.com>
2018-03-16 18:36:16 -0300

    follow through with bootstrap chapter

--- app/forms.py
+++ app/forms.py
@@ -6,27 +6,27 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField("usuário", validators=[DataRequired()])
-    password = PasswordField("senha", validators=[DataRequired()])
-    remember_me = BooleanField("lembrar de mim")
-    submit = SubmitField("entrar")
+    username = StringField("Usuário", validators=[DataRequired()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    remember_me = BooleanField("Lembrar de mim")
+    submit = SubmitField("Entrar")
 
 class RegistrationForm(FlaskForm):
-    username = StringField("usuário", validators=[DataRequired()])
-    email = StringField("email", validators=[DataRequired(), Email()])
+    username = StringField("Usuário", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     #entity = StringField("entidade governamental", validators=[DataRequired()])
     #subentity = StringField("sub-entidade governamental", validators=[DataRequired()])
-    password = PasswordField("senha", validators=[DataRequired()])
+    password = PasswordField("Senha", validators=[DataRequired()])
     password2 = PasswordField(
-        "repita a senha", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("registrar")
+        "Repita a senha", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Registrar")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("usuário já existe, escolha outro.")
+            raise ValidationError("Usuário já existe, escolha outro.")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError("este e-mail já foi cadastrado, por favor escolha outro.")
+            raise ValidationError("Este e-mail já foi cadastrado, por favor escolha outro.")
